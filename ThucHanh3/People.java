55776a94d57dda10c8df185aff7487042243c5c9
Konf1ic <114355460+Konf1ic@users.noreply.github.com>
2023-04-12 16:14:01 +0700

    Update People.java

--- ThucHanh3/People.java
+++ ThucHanh3/People.java
@@ -1,22 +1,26 @@
 package ThucHanh3;
 
-public class People {
+public class People {                     //Khai báo people
     private String name;
     private boolean gender;
     private int age;
+    //Khai báo các thuộc tính 'name','gender','age'
 
+    //Khởi tạo 0 tham số
     public People() {
         this.name = "Tên mặc định";
         this.gender = false;
         this.age = 0;
     }
 
+    //Khởi tạo 3 tham số
     public People(String name, boolean gender, int age) {
         this.name = name;
         this.gender = gender;
         this.age = age;
     }
 
+    //Getter\Setter
     public String getName() {
         return name;
     }
@@ -41,14 +45,17 @@ public class People {
         this.age = age;
     }
 
+    //Đ\N eat để hiển thị
     public void eat() {
         System.out.println("People eating......");
     }
 
+    //Đ\N sleep để hiển thị
     public void sleep(){
         System.out.println("Sleeping.....");
     }
 
+    // toString trả về giá trị
     @Override
     public String toString() {
         return "A people with name: "
